8550d4eba48490fee7af6c3861978bdc3ed2f85f
yonasketema <yonask.kitaw@gmail.com>
2024-06-11 22:28:02 +0300

    init project

---
+++ ezil.ts
@@ -0,0 +1,8 @@
+// TODO: node ezil "web" "https:/..."
+
+// TODO: scrap pdf ,web , youtube ->
+// TODO: save to memory vector db
+
+// TODO: summary , bullet point , ask my question -> prompt
+
+// TOOLS : scraper(web , youtube) ,
